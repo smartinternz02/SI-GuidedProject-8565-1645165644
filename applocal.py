4312c90d7f904a08df13176438c22024d50abf40
ATHIRA-AB <96428012+ATHIRA-AB@users.noreply.github.com>
2022-05-17 14:33:28 +0530

    Update and rename app1.py to applocal.py

--- applocal.py
+++ applocal.py
@@ -10,15 +10,6 @@ import json
 app = Flask(__name__)
 model = pickle.load(open('C:\\Users\\athir\\Desktop\\visa\\Visarf.pkl', 'rb'))
 
-
-# NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
-API_KEY = "qkiuYnigPT5H8XW33Plj6CfWV5oUqPjZ2I3noxm7PYEr"
-token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey": API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
-mltoken = token_response.json()["access_token"]
-
-header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
-
-
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -34,8 +25,7 @@ def predict():
     
     payload_scoring = {"input_data": [{"field": [['FULL_TIME_POSITION', 'PREVAILING_WAGE', 'YEAR','SOC_N']], "values": [input_features]}]}
 
-    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/9078763e-b479-4774-9abe-a28bdab485e9/predictions?version=2021-10-26', json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
-    print("Scoring response")
+
     pred=response_scoring.json()
     print(pred)
     output = pred['predictions'][0]['values'][0][0]
